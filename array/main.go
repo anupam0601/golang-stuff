745b276e557f504c7e92873d9f524dd49c0f9e53
Anupam <anupam@Anupams-MacBook-Pro.local>
2018-01-16 00:53:41 +0530

    basic go

---
+++ array/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Array of fixed size and type
+var anupam = [3]int{1, 2, 3}
+
+// undetermined array
+var debnath = [...]int{4, 5, 6, 7, 8}
+
+func main() {
+	q := [3]string{"go", "java", "python"}
+	fmt.Println("array- ", anupam)
+	fmt.Println("array2- ", q)
+}
