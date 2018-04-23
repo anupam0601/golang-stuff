a8cf5334ac3d7bbdec3122b9702c1933e6a0d712
Anupam <anupam@Anupams-MacBook-Pro.local>
2018-04-24 00:24:31 +0530

    worker pool with channels

--- slice/main.go
+++ slice/main.go
@@ -11,9 +11,13 @@ func main() {
 	fmt.Println(len(mySlice))
 	fmt.Println(cap(mySlice))
 	fmt.Println("------------------")
+	people := append(mySlice, 1, 2, 3)
+	fmt.Println(people)
+	fmt.Println(people[:1])
+	fmt.Println(people[1:])
 
-	for i := 0; i < 20; i++ {
-		mySlice = append(mySlice, i)
-		fmt.Println("Len: ", len(mySlice), "Capacity: ", cap(mySlice), "Value: ", mySlice[i])
-	}
+	// for i := 0; i < 20; i++ {
+	// 	mySlice = append(mySlice, i)
+	// 	fmt.Println("Len: ", len(mySlice), "Capacity: ", cap(mySlice), "Value: ", mySlice[i])
+	// }
 }
