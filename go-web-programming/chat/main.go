f1d2d53b000e87ccf8dfb5934f0399fac46a3549
adebnath1989@gmail.com <adebnath1989@gmail.com>
2018-07-28 08:57:50 +0530

    web programming sockets

--- go-web-programming/chat/main.go
+++ go-web-programming/chat/main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-				<title>Chat</title>
-				</head>
-				<body>
-				Let's chat!
-				</body>
-			</html>`))
-	})
-	// start the web server
-	if err := http.ListenAndServe(":8084", nil); err != nil {
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+
+	r := newRoom()
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+
 }
