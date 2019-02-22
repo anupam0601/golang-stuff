bd85ed90a3893b8eae7600acde4cceb99083d5f8
adebnath1989@gmail.com <adebnath1989@gmail.com>
2019-02-22 22:58:20 +0530

    Added logic for error channel struct for upstream upload

--- ints-perf-automation/main.go
+++ ints-perf-automation/main.go
@@ -3,19 +3,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"sync"
+	"time"
 )
 
 // total number of go routines to use at max
-const maxgoroutines = 10
+const maxgoroutines = 2
 
-func createFiles(a int){
+func createFiles(a string, chUpstream chan UpstreamChan){
 	fmt.Println("Creating file :>>  ", a)
+	f, err := os.Create(a)
+	if err != nil {
+		chUpstream <- UpstreamChan{"", err}
+		//fmt.Println(err)
+	}
+	//chUpstream <- a
+	chUpstream <- UpstreamChan{a, nil}
+	defer f.Close()
 	return
 }
 
+//Channel struct for upstream with data and error
+type UpstreamChan struct {
+	Output string
+	Err error
+}
+
 func main(){
-	var ch = make(chan int, 50) //It can be anything as long it is greater than maxgoroutines
+	var ch = make(chan string, 100) //It can be anything as long it is greater than maxgoroutines
+	var chUpstream = make(chan UpstreamChan, 100) //Error and output channel
 	var wg sync.WaitGroup
 
 	//This block starts maxgoroutines that wait for executing some job
@@ -29,16 +47,36 @@ func main(){
 					wg.Done() // if there is no file to create and the channel has been closed then end the goroutine
 					return
 				}
-				createFiles(a)
+				createFiles(a, chUpstream)
+				//time.Sleep(2 * time.Second)
 			}
 		}()
 	}
 
 	// Add the files to the channel , which is used as queue
 	for i:=0; i<50; i++{
-		ch <- i //add i to queue
+		t := strconv.Itoa(i)
+		ch <- t //add i to queue
 	}
 
 	close(ch) //This tells the goroutines there is nothing else to do, channel is closed
 	wg.Wait() //Wait for the goroutines to finish
+	close(chUpstream)
+	fmt.Println("Waiting.. 2 Secs....")
+	time.Sleep(2 * time.Second)
+
+	for r := range chUpstream{
+		if r.Err != nil {
+			fmt.Println(r.Err)
+		}
+		fmt.Println(r.Output)
+		fmt.Println(r)
+	}
+
+	//select {
+	//case msg1 := <-chUpstream:
+	//	fmt.Println(msg1)
+	//case msg2 := <-chUpstream:
+	//	fmt.Println(msg2)
+	//}
 }
