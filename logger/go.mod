373aff590842468b6d2aa1a16218637b3a07e4cc
ints-automation+1-at-324744861524 <anupam.debnath@syncron.com>
2019-10-21 00:43:48 +0530

    Created a flexible logger interface

---
+++ logger/go.mod
@@ -0,0 +1,8 @@
+module github.com/anupam0601/golang-stuff/logger
+
+require (
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.2.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
--- logger/logger.go
+++ logger/logger.go
@@ -1,41 +1,106 @@
 package logger
 
-import (
-	"github.com/sirupsen/logrus"
+import "errors"
+
+// A global variable so that log functions can be directly accessed
+var log Logger
+
+//Fields type to pass when we want to call WithFields for structured logging
+type Fields map[string]interface{}
+
+const (
+	//Debug has verbose message
+	Debug = "debug"
+	//Info is default log level
+	Info = "info"
+	//Warn is for logging messages about possible issues
+	Warn = "warn"
+	//Error is for logging errors
+	Error = "error"
+	//Fatal is for logging fatal messages. The sytem shutsdown after logging the message.
+	Fatal = "fatal"
 )
 
-// Event stores messages to log later, from our standard interface
-type Event struct {
-	id      int
-	message string
-}
+const (
+	InstanceZapLogger int = iota
+	InstanceLogrusLogger
+)
 
-// StandardLogger enforces specific log message formats
-type StandardLogger struct {
-	*logrus.Logger
-}
-
-// NewLogger initializes the standard logger
-func NewLogger() *StandardLogger {
-	var baseLogger = logrus.New()
-	var standardLogger = &StandardLogger{baseLogger}
-	standardLogger.Formatter = &logrus.JSONFormatter{}
-	return standardLogger
-}
-
-// Declare variables to store log messages as new Events
 var (
-	attributeNotPresent = Event{id: 3000, message: "Attribute not present: %s"}
-	step = Event{message:"STEP %s: %s"}
+	errInvalidLoggerInstance = errors.New("Invalid Logger Instance")
 )
 
-// AttributeNotPresent is a standard error message
-func (l *StandardLogger) AttributeNotPresent(attrName string) {
-	l.Errorf(attributeNotPresent.message, attrName)
+//Logger is the contract for the logger
+type Logger interface {
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+
+	Warnf(format string, args ...interface{})
+
+	Errorf(format string, args ...interface{})
+
+	Fatalf(format string, args ...interface{})
+
+	Panicf(format string, args ...interface{})
+
+	WithFields(keyValues Fields) Logger
 }
 
-// Step is an info level message to log test steps
-func(l * StandardLogger) Step(stepNumb string, stepMsg string){
-	//l.Info(attrName)
-	l.Infof(step.message, stepNumb, stepMsg)
+// Configuration stores the config for the logger
+// For some loggers there can only be one level across writers, for such the level of Console is picked by default
+type Configuration struct {
+	EnableConsole     bool
+	ConsoleJSONFormat bool
+	ConsoleLevel      string
+}
+
+//NewLogger returns an instance of logger
+func NewLogger(config Configuration, loggerInstance int) error {
+	switch loggerInstance {
+	case InstanceZapLogger:
+		logger, err := newZapLogger(config)
+		if err != nil {
+			return err
+		}
+		log = logger
+		return nil
+	case InstanceLogrusLogger:
+		logger, err := newLogrusLogger(config)
+		if err != nil {
+			return err
+		}
+		log = logger
+		return nil
+
+	default:
+		return errInvalidLoggerInstance
+	}
+}
+
+func Debugf(format string, args ...interface{}) {
+	log.Debugf(format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	log.Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	log.Fatalf(format, args...)
+}
+
+func Panicf(format string, args ...interface{}) {
+	log.Panicf(format, args...)
+}
+
+func WithFields(keyValues Fields) Logger {
+	return log.WithFields(keyValues)
 }
